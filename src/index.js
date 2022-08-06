1324a1f7b7e9de6146c62a6c968f63721e721963
Ganesh Ghadge <89139437+ganesh6465@users.noreply.github.com>
2022-08-06 12:15:26 +0530

    used letest react syntax

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement)
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
