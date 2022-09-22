0eb1112d9b1fb99bb6f8d08c0be646122de40bf5
Ganesh Ghadge <89139437+ganraj21@users.noreply.github.com>
2022-09-22 12:07:19 +0530

    Contact page building start

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,6 +6,12 @@ function Contact() {
     <div>
       <div className="contact_page">
         <h2>This is contact page</h2>
+<div className="contact_container">
+<div className="form_container_left">
+<h2>Love to hare from you Get in touch</h2>
+</div>
+<div className="form_container_right"></div>
+</div>
       </div>
     </div>
   );
