7c92561b90eab42a528eb4f62c81da1608e7cf05
Ganesh Ghadge <89139437+ganesh6465@users.noreply.github.com>
2022-12-06 23:54:19 +0530

    change #301

---
+++ src/pages/components/Education.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Education.css";
+function Education() {
+  return (
+    <>
+      <div className="educ_container"></div>
+    </>
+  );
+}
+
+export default Education;
