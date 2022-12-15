77f8aaa0c1c4ba653d6004fbff4c74457ac9850f
Ganesh Ghadge <89139437+ganesh6465@users.noreply.github.com>
2022-12-15 20:31:27 +0530

    change #328

--- src/pages/Project_exp.jsx
+++ src/pages/Project_exp.jsx
@@ -1,90 +1,8 @@
 import React from "react";
 import ProjHome from "./components/ProjHome";
 import "./Project_exp.css";
+import proj_data from "./assets/ProjectsData";
 const Project_exp = () => {
-  const proj_data = [
-    {
-      id: 1,
-      project_name: "Dental-Clinic Web-Application",
-      proj_creation_date: "Nov 2022",
-      project_image: "asdasd",
-      proj_description: "",
-      proj_code_like: "",
-      proj_live_link: "",
-    },
-    {
-      id: 2,
-      project_name: "Cloud Resume Generator",
-      proj_creation_date: "Oct 2022",
-      project_image: "sdfjwer3sd",
-      proj_description: "",
-      proj_code_like: "",
-      proj_live_link: "",
-    },
-    {
-      id: 3,
-      project_name: "HyperLogin App",
-      proj_creation_date: "Nov 2022",
-      project_image: "asdasddfusdf",
-      proj_description: "",
-      proj_code_like: "",
-      proj_live_link: "",
-    },
-    {
-      id: 4,
-      project_name: "Open Profile Card / Mini-Portfolio",
-      proj_creation_date: "feb 2022",
-      project_image: "assduersdasd",
-      proj_description: "",
-      proj_code_like: "",
-      proj_live_link: "",
-    },
-    {
-      id: 5,
-      project_name: "Quotes generatorBot",
-      proj_creation_date: "June 2022",
-      project_image: "asdasdkfbsd",
-      proj_description: "",
-      proj_code_like: "",
-      proj_live_link: "",
-    },
-    {
-      id: 6,
-      project_name: " NativeWeather app",
-      proj_creation_date: "March 2022",
-      project_image: "asdasddfusdf",
-      proj_description: "",
-      proj_code_like: "",
-      proj_live_link: "",
-    },
-    {
-      id: 7,
-      project_name: "Metabanking system",
-      proj_creation_date: "Dec 2021",
-      project_image: "asdasddfusdf",
-      proj_description: "",
-      proj_code_like: "",
-      proj_live_link: "",
-    },
-    {
-      id: 8,
-      project_name: "Infinineu-calculator",
-      proj_creation_date: "Dec 2021",
-      project_image: "asdasddfusdf",
-      proj_description: "",
-      proj_code_like: "",
-      proj_live_link: "",
-    },
-    {
-      id: 9,
-      project_name: "Quick To-Do-List Web-Application",
-      proj_creation_date: "Dec 2021",
-      project_image: "asdasddfusdf",
-      proj_description: "",
-      proj_code_like: "",
-      proj_live_link: "",
-    },
-  ];
   return (
     <div>
       <div className="work_page">
@@ -94,7 +12,8 @@ const Project_exp = () => {
         <div className="project_work_page">
           {proj_data.map((e, index) => (
             <div className="section" key={index}>
-              <div className="container">
+              <div className="pcontainer">
+                <img src={e.project_image} alt="project Images" />
                 <div className="proj_header">
                   <h3>{e.project_name}</h3>
                   <h4>{e.proj_creation_date}</h4>
---
+++ src/pages/assets/ProjectsData.js
@@ -0,0 +1,95 @@
+import dental_img from "../../Image/proj_imgs/clinic_img.jpg";
+import resume_img from "../../Image/proj_imgs/resume_img.webp";
+import login_img from "../../Image/proj_imgs/login_img.jpg";
+import profile_img from "../../Image/proj_imgs/profile_img.jpg";
+import quotes_img from "../../Image/proj_imgs/quotes_img.jpg";
+import weather_img from "../../Image/proj_imgs/weather_img.jpg";
+import banking_img from "../../Image/proj_imgs/banking_img.jpg";
+import calci_img from "../../Image/proj_imgs/calci_img.jpg";
+import todo_img from "../../Image/proj_imgs/todo_img.jpg";
+
+const proj_data = [
+  {
+    id: 1,
+    project_name: "Dental-Clinic Web-Application",
+    proj_creation_date: "Nov 2022",
+    project_image: dental_img,
+    proj_description: "",
+    proj_code_like: "",
+    proj_live_link: "",
+  },
+  {
+    id: 2,
+    project_name: "Cloud Resume Generator",
+    proj_creation_date: "Oct 2022",
+    project_image: resume_img,
+    proj_description: "",
+    proj_code_like: "",
+    proj_live_link: "",
+  },
+  {
+    id: 3,
+    project_name: "HyperLogin App / Kotlin",
+    proj_creation_date: "Nov 2022",
+    project_image: login_img,
+    proj_description: "",
+    proj_code_like: "",
+    proj_live_link: "",
+  },
+  {
+    id: 4,
+    project_name: "Open Profile Card / Mini-Portfolio",
+    proj_creation_date: "feb 2022",
+    project_image: profile_img,
+    proj_description: "",
+    proj_code_like: "",
+    proj_live_link: "",
+  },
+  {
+    id: 5,
+    project_name: "Quotes generatorBot",
+    proj_creation_date: "June 2022",
+    project_image: quotes_img,
+    proj_description: "",
+    proj_code_like: "",
+    proj_live_link: "",
+  },
+  {
+    id: 6,
+    project_name: " NativeWeather app",
+    proj_creation_date: "March 2022",
+    project_image: weather_img,
+    proj_description: "",
+    proj_code_like: "",
+    proj_live_link: "",
+  },
+  {
+    id: 7,
+    project_name: "Metabanking system",
+    proj_creation_date: "Dec 2021",
+    project_image: banking_img,
+    proj_description: "",
+    proj_code_like: "",
+    proj_live_link: "",
+  },
+  {
+    id: 8,
+    project_name: "Infinineu-calculator",
+    proj_creation_date: "Dec 2021",
+    project_image: calci_img,
+    proj_description: "",
+    proj_code_like: "",
+    proj_live_link: "",
+  },
+  {
+    id: 9,
+    project_name: "Quick To-Do-List Web-Application",
+    proj_creation_date: "Dec 2021",
+    project_image: todo_img,
+    proj_description: "",
+    proj_code_like: "",
+    proj_live_link: "",
+  },
+];
+
+export default proj_data;
