c4e125a83dde02310832868ce0b7fe55bd6824cc
Ganesh Ghadge <89139437+ganesh6465@users.noreply.github.com>
2022-12-17 13:29:10 +0530

    CHANGE LOGO

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { HashLink } from "react-router-hash-link";
 import "./Navbar.css";
+import nav_logo from "../Image/Profile_imgs/nav_logo.png";
 import "font-awesome/css/font-awesome.min.css";
 import my_navbar_data from "./assets/NavData";
 
@@ -19,7 +20,8 @@ const Navbar = () => {
       <div className="nav-container">
         <div className="logo">
           <HashLink to="/#home_page">
-            <h3>GhadgeGB</h3>
+            {/* <h3>GhadgeGB</h3> */}
+            <img src={nav_logo} alt="" />
           </HashLink>
         </div>
         <div className={isActive ? "active_links" : "links"}>
