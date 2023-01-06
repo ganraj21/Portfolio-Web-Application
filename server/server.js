ed815bb873d09b11cc9a61ecfed59dde931335d0
Ganesh Ghadge <89139437+ganesh6465@users.noreply.github.com>
2023-01-06 16:26:57 +0530

    removed .env file

--- server/server.js
+++ server/server.js
@@ -8,10 +8,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const MONGO_URL =
-  "mongodb+srv://ganesh21:N858ky4NjqKTApJs@portwebsite.6i5isja.mongodb.net/ums?retryWrites=true&w=majority";
 mongoose
-  .connect(MONGO_URL, {
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
