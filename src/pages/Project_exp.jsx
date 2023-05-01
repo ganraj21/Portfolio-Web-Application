c88908f8e8d371de6d825c3f911f82864706cc46
Ganesh Ghadge <89139437+ganesh6465@users.noreply.github.com>
2023-05-01 22:37:49 +0530

    card controller added

--- src/pages/Project_exp.jsx
+++ src/pages/Project_exp.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import ProjHome from "./components/ProjHome";
+import { HashLink } from "react-router-hash-link";
 import "./Project_exp.css";
 import proj_data from "./assets/ProjectsData";
+
 const Project_exp = () => {
+  const [showAllCards, setShowAllCards] = useState(false);
+  const visibleCards = proj_data.slice(0, 5);
+
   return (
     <div>
       <div className="work_page" id="my-work">
@@ -10,7 +15,7 @@ const Project_exp = () => {
           <ProjHome />
         </div>
         <div className="project_work_page">
-          {proj_data.map((e, index) => (
+          {visibleCards.map((e, index) => (
             <div
               className="section"
               key={index}
@@ -37,6 +42,58 @@ const Project_exp = () => {
               </div>
             </div>
           ))}
+
+          {showAllCards && (
+            <>
+              {proj_data.slice(6).map((e, index) => (
+                <div
+                  className="section"
+                  key={index}
+                  data-aos="fade-up"
+                  data-aos-duration="2000"
+                >
+                  <div className="pcontainer">
+                    <img src={e.project_image} alt="project Images" />
+                    <div className="proj_header">
+                      <h3>{e.project_name}</h3>
+                      <h4>{e.proj_creation_date}</h4>
+                    </div>
+                    <div className="proj_descriptions">
+                      <p>{e.proj_description}</p>
+                    </div>
+                    <div className="proj_btns">
+                      <a href={e.proj_code_like}>
+                        <button className="proj_code">Project Code</button>
+                      </a>
+                      <a href={e.proj_live_link}>
+                        <button className="proj_live">Live Project</button>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              ))}
+              <HashLink
+                onClick={() => setShowAllCards(false)}
+                to={"/#my-work"}
+                data-aos="fade-down"
+                data-aos-duration="2000"
+                className="view__btn"
+              >
+                Okey,I got it
+              </HashLink>
+            </>
+          )}
+
+          {!showAllCards && (
+            <button
+              onClick={() => setShowAllCards(true)}
+              data-aos="fade-down"
+              data-aos-duration="2000"
+              className="view__btn"
+            >
+              View All
+            </button>
+          )}
         </div>
       </div>
     </div>
